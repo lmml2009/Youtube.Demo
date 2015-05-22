ebe7c9bc9d439e5a5fc037b5f9475c3ac157c3ef
Kai Li <nutjob.kli@gmail.com>
2015-05-22 11:14:51 -0700

    initial commit

---
+++ src/com/glogou/Main.java
@@ -0,0 +1,18 @@
+package com.glogou;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello,github!");
+
+        int x = 5;
+        int y=6;
+    }
+}
+
+class Calculator{
+    public int add(int x, int y){
+        int result = x+y;
+        return result;
+    }
+}
